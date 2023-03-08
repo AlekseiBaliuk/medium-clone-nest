9ff2ea806b6153273d3f220977536fed8de82190
Aleksei Baliuk <aleksred.b@gmail.com>
2023-03-08 11:51:34 +0200

    add

---
+++ src/user/user.controller.ts
@@ -0,0 +1,67 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Put,
+  UseGuards,
+  UsePipes,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/createUser.dto';
+import { LoginUserDto } from './dto/login.dto';
+import { UserResponseInterface } from './types/userResponse.interface';
+import { UserService } from './user.service';
+import { User } from './decorators/user.decorator';
+import { UserEntity } from './user.entity';
+import { AuthGuard } from './guards/auth.guard';
+import { UpdateUserDto } from './dto/updateUser.dto';
+import { BackendValidationPipe } from '@app/shared/pipes/backendValidation.pipe';
+
+@Controller()
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post('users')
+  @UsePipes(new BackendValidationPipe())
+  async createUser(
+    @Body('user') createUserDto: CreateUserDto,
+  ): Promise<UserResponseInterface> {
+    const user = await this.userService.createUser(createUserDto);
+    return this.userService.buildUserResponse(user);
+  }
+
+  @Post('users/login')
+  @UsePipes(new BackendValidationPipe())
+  async login(
+    @Body('user') loginUserDto: LoginUserDto,
+  ): Promise<UserResponseInterface> {
+    const user = await this.userService.login(loginUserDto);
+    return this.userService.buildUserResponse(user);
+  }
+
+  @Get('user')
+  @UseGuards(AuthGuard)
+  async currentUser(
+    // @Req() request: ExpressRequestInterface,
+    @User() user: UserEntity,
+    // @User('id') currentUserId: number,
+  ): Promise<UserResponseInterface> {
+    // console.log(user);
+    // console.log(currentUserId);
+    return this.userService.buildUserResponse(user);
+  }
+
+  @Put('user')
+  @UseGuards(AuthGuard)
+  async updateCurrentUser(
+    @User('id') currentUserId: number,
+    @Body('user') updateUserDto: UpdateUserDto,
+  ): Promise<UserResponseInterface> {
+    const user = await this.userService.updateUser(
+      currentUserId,
+      updateUserDto,
+    );
+
+    return this.userService.buildUserResponse(user);
+  }
+}
